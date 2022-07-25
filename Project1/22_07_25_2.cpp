9f95e7d64407dc7438b5cefb64a69a9740c95a34
YooByeongJu <dbqudwn011@naver.com>
2022-07-25 22:34:19 +0900

    백준_3052_나머지_algorithm의 sort를 이용하여 정렬후 unique를 사용하여 중복값을 뒤로보내고 erase를 사용하여 삭제

---
+++ Project1/22_07_25_2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//	int a;
+//	vector<int> arr;
+//
+//	for (int i = 0; i < 10; i++) {
+//		cin >> a;
+//		a %= 42;
+//		arr.push_back(a);
+//	}
+//
+//	sort(arr.begin(), arr.end());
+//	arr.erase(unique(arr.begin(), arr.end()), arr.end());
+//
+//	cout << arr.size();
+//	return 0;
+//}
