ed8aadfa02ef5deda0fb718ec4dc6a1a7cec05da
YooByeongJu <dbqudwn011@naver.com>
2022-08-02 23:18:54 +0900

    백준 10809, 1152 1152 - 왜 틀렸나 다시한번 따져보기

---
+++ Project1/22_08_02.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	string s;
+//	int a = 'a', n;
+//	int al[26] = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
+//	cin >> s;
+//	
+//	for (int i = 0; i < s.size(); i++) {
+//		n = s[i] - a;
+//		if (al[n] != -1)
+//			continue;
+//		al[n] = i;
+//		
+//	}
+//
+//	for (int i = 0; i < 26; i++) {
+//		cout << al[i] << " ";
+//	}
+//
+//}
