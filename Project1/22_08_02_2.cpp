defc834a273eaa9505d9b9af31bdfe2006c16c84
YooByeongJu <dbqudwn011@naver.com>
2022-08-04 21:19:43 +0900

    프로그래머스_카카오_다트게임

--- Project1/22_08_02_2.cpp
+++ Project1/22_08_02_2.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-	string s;
-	int count = 1;
-	getline(cin, s);
-	if (s.size() > 1000000)
-		return 0;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == ' ')
-		{
-			if (i == 0 || i == s.size() - 1)
-				continue;
-			count++;
-		}
-
-	}
-	
-	cout << count;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	string s;
+//	int count = 1;
+//	getline(cin, s);
+//	if (s.size() > 1000000)
+//		return 0;
+//	for (int i = 0; i < s.size(); i++) {
+//		if (s[i] == ' ')
+//		{
+//			if (i == 0 || i == s.size() - 1)
+//				continue;
+//			count++;
+//		}
+//
+//	}
+//	
+//	cout << count;
+//}
