037b001646717acea261a73eedcb01f682265edc
YooByeongJu <dbqudwn011@naver.com>
2023-02-28 23:13:38 +0900

    2.28 프로그래머스

---
+++ Project1/2_28.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//vector<int> solution(string s) {
+//    vector<int> answer;
+//    answer.push_back(0);
+//    answer.push_back(0);
+//    while (s != "1") {
+//        int count = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s[i] == '0') ++count;
+//        }
+//        int len = s.length() - count;
+//        cout << "전체 길이 : " <<s.length() << endl << "0의 갯수 : " << count << "1의 갯수 : " << "   " << len << endl;
+//        string st="";
+//        while (len != 0) {
+//
+//            if (len == 1) {
+//                st += '1';
+//                break;
+//            }
+//
+//            if (len / 2 == 1) {
+//                st += ('0' + len % 2);
+//                st += '1';
+//                len = 0;
+//            }
+//            else{
+//                st += ('0' + len % 2);
+//                len /= 2;
+//            }
+//        }
+//        answer[0] += 1;
+//        answer[1] += count;
+//        s = st;
+//    }
+//    return answer;
+//}
+
+int main() {
+
+}
