73c057198816c10ba5d1f0b009db81382cc6eeb0
YooByeongJu <dbqudwn011@naver.com>
2023-07-12 20:17:15 +0900

    0712

--- Project1/07_11.cpp
+++ Project1/07_11.cpp
@@ -26,93 +26,93 @@
 //    return answer;
 //}
 
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iostream>
-using namespace std;
-
-string solution(string m, vector<string> musicinfos) {
-    string answer = "";
-
-    for (int i = 0; i < musicinfos.size(); i++) {
-        cout << "i : " << i << endl;
-
-        stringstream ss(musicinfos[i]);
-        string token;
-        vector <string> result;
-
-        while (getline(ss, token, ',')) {
-            if (token != ",") {
-                result.push_back(token);
-            }
-            else {
-                ss.unget();
-            }
-        }
-
-        int h = stoi(result[1].substr(1)) - stoi(result[0].substr(1));
-        int mi = stoi(result[1].substr(3,2)) - stoi(result[0].substr(3,2));
-        mi += h * 60;
-        cout << mi << endl;
-
-        int size = 0;
-        for (int num = 0; num < result[3].size(); num++) {
-            if (result[3][num] == '#') continue;
-            else size++;
-        }
-        string str = "";
-
-        if (mi > size) {
-            for (int j = 0; j < mi / size; j++) {
-                str += result[3];
-            }
-            if (mi % size < size - 1) {
-                if (result[3][mi % size + 1] != '#')
-                    str += result[3].substr(0, mi % size);
-                else
-                    str += result[3].substr(0, mi % size + 1);
-            }
-            else
-                str += result[3].substr(0, mi % size);
-        }
-        else if (mi < size) {
-            if(result[3][mi + 1]=='#')
-                str += result[3].substr(0,mi+1);
-            else
-                str += result[3].substr(0, mi);
-            
-        }
-        else str = result[3];
-
-        cout <<"str: "<< str <<"  | m: "<<m<< endl;
-
-        size_t found = str.find(m);
-        if (found != string::npos) {
-            cout << "found : " << found + m.size() << endl;
-            if (str[(found+m.size())] == '#')
-                continue;
-            else {
-                if (answer.length() < str.length())
-                    answer = result[2];
-            }
-        }  
-
-    }
-    if (answer == "")
-        answer = "(None)";
-    return answer;
-}
-
-int main() {
-   
-    string m = "ABCDEFG";
-    vector<string> v = { "12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF" };
-    
-    cout << "결과: " << solution(m, v) << endl << endl;
-
-    string m1 = "ABC";
-    vector<string> v1 = { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" };
-    
-    cout << "결과: " << solution(m1, v1);
-}
+//#include <string>
+//#include <vector>
+//#include <sstream>
+//#include <iostream>
+//using namespace std;
+//
+//string solution(string m, vector<string> musicinfos) {
+//    string answer = "";
+//
+//    for (int i = 0; i < musicinfos.size(); i++) {
+//        cout << "i : " << i << endl;
+//
+//        stringstream ss(musicinfos[i]);
+//        string token;
+//        vector <string> result;
+//
+//        while (getline(ss, token, ',')) {
+//            if (token != ",") {
+//                result.push_back(token);
+//            }
+//            else {
+//                ss.unget();
+//            }
+//        }
+//
+//        int h = stoi(result[1].substr(1)) - stoi(result[0].substr(1));
+//        int mi = stoi(result[1].substr(3,2)) - stoi(result[0].substr(3,2));
+//        mi += h * 60;
+//        cout << mi << endl;
+//
+//        int size = 0;
+//        for (int num = 0; num < result[3].size(); num++) {
+//            if (result[3][num] == '#') continue;
+//            else size++;
+//        }
+//        string str = "";
+//
+//        if (mi > size) {
+//            for (int j = 0; j < mi / size; j++) {
+//                str += result[3];
+//            }
+//            if (mi % size < size - 1) {
+//                if (result[3][mi % size + 1] != '#')
+//                    str += result[3].substr(0, mi % size);
+//                else
+//                    str += result[3].substr(0, mi % size + 1);
+//            }
+//            else
+//                str += result[3].substr(0, mi % size);
+//        }
+//        else if (mi < size) {
+//            if(result[3][mi + 1]=='#')
+//                str += result[3].substr(0,mi+1);
+//            else
+//                str += result[3].substr(0, mi);
+//            
+//        }
+//        else str = result[3];
+//
+//        cout <<"str: "<< str <<"  | m: "<<m<< endl;
+//
+//        size_t found = str.find(m);
+//        if (found != string::npos) {
+//            cout << "found : " << found + m.size() << endl;
+//            if (str[(found+m.size())] == '#')
+//                continue;
+//            else {
+//                if (answer.length() < str.length())
+//                    answer = result[2];
+//            }
+//        }  
+//
+//    }
+//    if (answer == "")
+//        answer = "(None)";
+//    return answer;
+//}
+//
+//int main() {
+//   
+//    string m = "ABCDEFG";
+//    vector<string> v = { "12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF" };
+//    
+//    cout << "결과: " << solution(m, v) << endl << endl;
+//
+//    string m1 = "ABC";
+//    vector<string> v1 = { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" };
+//    
+//    cout << "결과: " << solution(m1, v1);
+//}
---
+++ Project1/07_12.cpp
@@ -0,0 +1,112 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//
+//
+//vector<int> solution(vector<int> sequence, int k) {
+//    vector<int> answer;
+//    vector<pair<int, int>> ans;
+//    int size = sequence.size();
+//    int min = 1000001;
+//
+//    if (sequence[0] == k) {
+//        ans.push_back(make_pair(0, 0));
+//    }
+//
+//    for (int i = 0; i < size - 1; i++) {
+//        long long int sum = sequence[i];
+//
+//        for (int j = i+1; j < size; j++) {
+//            if (sequence[j] == k) {
+//                ans.push_back(make_pair(j, j));
+//                break;
+//            }
+//
+//            sum += sequence[j];
+//
+//            if (sum < k) continue;
+//            else if (sum > k) break;
+//            else {
+//                if (min > j - i) {
+//                    min = j - i;
+//                    ans.push_back(make_pair(i, j));
+//                    break;
+//                }
+//            }
+//
+//        }
+//    }
+//    answer.push_back(ans.back().first);
+//    answer.push_back(ans.back().second);
+//    return answer;
+//}
+//
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> sequence, int k) {
+//    int s = 0, e = 0;
+//    int sum = sequence[0]; // 부분 수열의 합
+//    int subLen = sequence.size() + 1; // 부분 수열의 길이
+//    pair<int, int> result; // 부분 수열의 시작 인덱스와 마지막 인덱스를 담은 객체
+//
+//    while (s <= e && e < sequence.size()) {
+//        if (sum < k) sum += sequence[++e];
+//        else if (sum == k) {
+//            if (e - s + 1 < subLen) { // 길이가 더 짧은 수열이면
+//                subLen = e - s + 1;
+//                result = { s, e };
+//            }
+//            else if (e - s + 1 == subLen) {
+//                if (s < result.first) { // 시작 인덱스가 더 작으면
+//                    result = { s, e };
+//                }
+//            }
+//
+//            sum -= sequence[s++];
+//        }
+//        else sum -= sequence[s++];
+//    }
+//
+//    return { result.first, result.second };
+//}
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include<iostream>
+using namespace std;
+
+bool check(string a, string b) {
+    if (a[0] == b[0])
+        return a.length() < b.length();
+    return a > b;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> str;
+    int size = numbers.size();
+    for (int i = 0; i < size; i++) {
+        str.push_back(to_string(numbers[i]));
+    }
+    sort(str.begin(), str.end(), check);
+
+    for (int i = 0; i < size; i++) {
+        cout << str[i] << "  ";
+        answer += str[i];
+    }
+    return answer;
+}
+
+
+int main() {
+    vector<int>v = { 3, 30, 34, 5, 9,10,11,1 };
+    solution(v);
+}
